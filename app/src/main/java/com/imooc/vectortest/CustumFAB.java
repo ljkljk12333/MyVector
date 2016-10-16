b3cde4a54002253327fac773d41354d8bf07e37f
ljkljk12333 <ljkljk12333@163.com>
2016-10-16 16:38:26 +0800

    1.0

---
+++ app/src/main/java/com/imooc/vectortest/CustumFAB.java
@@ -0,0 +1,115 @@
+package com.imooc.vectortest;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.StateListDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.util.AttributeSet;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+/**
+ * Created by J.Li on 2016/9/19.
+ */
+public class CustumFAB extends ImageButton {
+
+    Context mContect;
+    int bg_Color;
+    int bg_Color_Pressed;
+
+    public CustumFAB(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mContect=context;
+        init(attrs);
+    }
+
+    public CustumFAB(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        mContect=context;
+        init(attrs);
+    }
+
+    public CustumFAB(Context context) {
+        super(context);
+        mContect=context;
+        init();
+    }
+
+    private void init(AttributeSet attrs){
+        Resources.Theme theme=mContect.getTheme();
+        TypedArray array=theme.obtainStyledAttributes(attrs,R.styleable.FAB,0,0);
+
+        try {
+            setBg_Color(array.getColor(R.styleable.FAB_bg_color,Color.BLUE));
+
+            setBg_Color_Pressed(array.getColor(R.styleable.FAB_bg_color_pressed,Color.GRAY));
+
+            StateListDrawable sld=new StateListDrawable();
+            sld.addState(new int[]{android.R.attr.state_pressed},createButton(bg_Color_Pressed));
+            sld.addState(new int[]{},createButton(bg_Color));
+            setBackground(sld);
+        }catch (Exception e){
+
+        }finally {
+            array.recycle();
+        }
+    }
+
+    private void init(){
+        Resources.Theme theme=mContect.getTheme();
+
+        try {
+            setBg_Color(Color.BLUE);
+
+            setBg_Color_Pressed(Color.GRAY);
+
+            StateListDrawable sld=new StateListDrawable();
+            sld.addState(new int[]{android.R.attr.state_pressed},createButton(bg_Color_Pressed));
+            sld.addState(new int[]{},createButton(bg_Color));
+            setBackground(sld);
+        }catch (Exception e){
+
+        }
+    }
+
+    public void setBg_Color(int bg_Color) {
+        this.bg_Color =bg_Color;
+    }
+
+    public void setBg_Color_Pressed(int bg_Color_Pressed) {
+        this.bg_Color_Pressed = bg_Color_Pressed;
+    }
+
+    private Drawable createButton(int color){
+        OvalShape ovalShape=new OvalShape();
+        ShapeDrawable shapeDrawable=new ShapeDrawable(ovalShape);
+        setWillNotDraw(false);
+        shapeDrawable.getPaint().setColor(color);
+
+        OvalShape ovalShape1=new OvalShape();
+        ShapeDrawable shapeDrawable1=new ShapeDrawable(ovalShape1);
+        shapeDrawable1.setShaderFactory(new ShapeDrawable.ShaderFactory() {
+            @Override
+            public Shader resize(int width, int height) {
+                LinearGradient linearGradient=new LinearGradient(0,0,0,height,
+                        new int[]{
+                                Color.WHITE,
+                                Color.LTGRAY,
+                                Color.GRAY
+                        },null,Shader.TileMode.REPEAT);
+                return linearGradient;
+            }
+        });
+        LayerDrawable layerDrawable=new LayerDrawable(new Drawable[]{shapeDrawable1,shapeDrawable});
+        layerDrawable.setLayerInset(0,1,1,0,0);
+        layerDrawable.setLayerInset(1,0,0,1,1);
+        return layerDrawable;
+    }
+}
---
+++ app/src/main/java/com/imooc/vectortest/MainActivity.java
@@ -0,0 +1,74 @@
+package com.imooc.vectortest;
+
+import android.animation.Animator;
+import android.app.Activity;
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
+import android.view.View;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageView imageView;
+
+    CustumFAB custumFAB;
+
+
+    boolean isOK=false;
+
+    static {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = (ImageView) findViewById(R.id.image);
+        imageView.setOnClickListener(MainActivity.this);
+
+        custumFAB = (CustumFAB)findViewById(R.id.btn_Add_Device);
+        custumFAB.setImageResource(R.drawable.vector_searcher);
+        custumFAB.setOnClickListener(MainActivity.this);
+
+//        ((ImageView) findViewById(R.id.image_anim_vector)).setOnClickListener(MainActivity.this);
+//        ((ImageView) findViewById(R.id.image_earth_moon_system)).setOnClickListener(MainActivity.this);
+    }
+
+    private void animate(ImageView tempImageView) {
+        Drawable drawable = tempImageView.getDrawable();
+        if (drawable instanceof Animatable) {
+            ((Animatable) drawable).start();
+        }
+    }
+
+    private void stopAnimate(ImageView tempImageView) {
+        Drawable drawable = tempImageView.getDrawable();
+        if (drawable instanceof Animatable) {
+            if(((Animatable) drawable).isRunning()) {
+                ((Animatable) drawable).stop();
+            }
+        }
+    }
+    @Override
+    public void onClick(View v) {
+        if(v instanceof CustumFAB) {
+            stopAnimate((CustumFAB) v);
+            if(!isOK){
+                ((CustumFAB) v).setImageResource(R.drawable.vector_searcher);
+            }else {
+                ((CustumFAB) v).setImageResource(R.drawable.vector_searcher2);
+            }
+            animate((CustumFAB) v);
+            isOK=!isOK;
+        }else if(v instanceof ImageView){
+            animate((ImageView) v);
+        }
+    }
+}
